3b758bba170b10d01f8515d2a61c710f2fb9690c
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2024-11-24 14:19:17 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'ReactDateRange',
+      fileName: (format) => `react-date-range.${format}.js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
