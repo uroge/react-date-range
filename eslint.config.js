1f811c8b820c856f8fcc0f355afa6f4c4cc66112
uros.milosavljevic <urosmilosavljevic63@gmail.com>
2024-11-25 15:37:57 +0100

    fix eslint extension

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config({ ignores: ['dist'] }, eslintConfigPrettier, {
-  extends: [js.configs.recommended, ...{ ...tseslint.configs.recommended }],
+  extends: [js.configs.recommended, tseslint.configs.recommended],
   files: ['**/*.{ts,tsx}'],
   languageOptions: {
     ecmaVersion: 2020,
